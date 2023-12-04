f49a02baf9f1a05b4cb2a1a04d90978dafac18cf
reon0921 <reonshankou40@gmail.com>
2023-12-04 19:21:11 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-   Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'items/index'
+  root to: "items#index"
+  resources :items, only: [:index]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  Defines the root path route ("/")
-  root "articles#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
