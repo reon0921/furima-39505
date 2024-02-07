176e06f4739ae79e9746d0ab9eb76cdc03b055cd
reon0921 <reonshankou40@gmail.com>
2024-02-07 20:10:56 +0900

    提出

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,17 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # ユーザーが正常に保存された場合の処理
+      redirect_to @user
+    else
+      # ユーザーの保存が失敗した場合の処理
+      render 'new'
+    end
+  end
+
   def basic_auth
     
     authenticate_or_request_with_http_basic do |username, password|
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,16 @@
 class User < ApplicationRecord
-  #validates :name, presence: true
-  #validates :email, presence: true
-  #validates :email, presence: true, uniqueness: true
-  #validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "は有効なメールアドレスの形式で入力してください" }
-  #validates :password, presence: true
-  #validates :password, presence: true, length: { minimum: 6 }
-  #validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" }
-  #validates :password, presence: true
-  #validates :password_confirmation, presence: true
-  #validates_confirmation_of :password
-  #validates :name_name, presence: true
-  #validates :first_name, presence: true
-  #validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください" }
-  #validates :first_name_kana, presence: true
-  #validates :name_kana, presence: true, format: { with: /\A[ァ-ンー－]+\z/, message: "は全角カタカナで入力してください" }
-  #validates :birthday, presence: true
-  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true
+  #validate :password_match?
+  validates :surname_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :name_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :surname_kana_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :kana_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+
+
   # user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
 
 
