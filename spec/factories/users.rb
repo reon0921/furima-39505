cf4eacfce588a88af2d08fbc1b5bc9a97eaf6139
reon0921 <reonshankou40@gmail.com>
2024-02-10 21:57:43 +0900

    修正８

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
     email { "test@example.com" }
-    password { "password" }
+    password { 'password' }
+    password_confirmation { 'password' }
     surname_name { '山田' } 
     name_name { '太郎' }
     surname_kana_name { 'ヤマダ' } 
     kana_name { 'タロウ' } 
     birthday { '2000-01-01' }
-
   end
 end
