af702803773e4ea2404cd1b5d48f604b096fb458
reon0921 <140514422+reon0921@users.noreply.github.com>
2024-04-06 22:06:39 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
- before_action :authenticate_user!, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
   before_action :set_item, only: [:edit, :update]
   before_action :restrict_edit, only: [:edit, :update]
-  
+
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -23,19 +23,23 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   def show
     @item = Item.find(params[:id])
   end
+
   def edit
     @item = Item.find(params[:id])
   end
+
   def update
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
+
   private
 
   def set_item
@@ -43,6 +47,8 @@ class ItemsController < ApplicationController
   end
 
   def restrict_edit
+    @item = Item.find(params[:id])
+    if @item.user_id != current_user.id
       redirect_to root_path
     end
   end
@@ -50,3 +56,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :availability_id, :expected_shipping_date_id, :prefecture_id, :text, :image, :expected_shipping_date_id).merge(user_id: current_user.id)
   end
+end
