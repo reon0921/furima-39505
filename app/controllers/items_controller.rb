7171cf47919770825a601d6626d02137b232b6fe
reon0921 <140514422+reon0921@users.noreply.github.com>
2024-04-07 16:17:27 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :restrict_edit, only: [:edit, :update]
-
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -25,11 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
