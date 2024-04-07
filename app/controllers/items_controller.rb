664872472451c839f1af6d9d461a248d91e9b449
reon0921 <140514422+reon0921@users.noreply.github.com>
2024-04-07 18:09:04 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :restrict_edit, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :restrict_edit, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -34,6 +34,13 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show, status: :unprocessable_entity
     end
   end
 
